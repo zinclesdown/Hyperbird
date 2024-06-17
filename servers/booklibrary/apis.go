b9bc536121960a5d04f9f6c76c643bf69175a381
zinclesdown <zinclesdown@gmail.com>
2024-06-18 03:44:38 +0800

    增加air的配置文件、编写更多api

--- servers/booklibrary/apis.go
+++ servers/booklibrary/apis.go
@@ -12,29 +12,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// 用户API接口,供前端用户调用
-type userAPI interface {
-	apiLogin(c *gin.Context)         // 登录接口，返回用户上下文
-	apiGetAllBookIds(c *gin.Context) // 获取所有书籍 ID
-	apiGetBookInfo(c *gin.Context)   // 获取书籍信息
-	apiGetBookFile(c *gin.Context)   // 获取书籍文件
-	apiAlive(c *gin.Context)         // 保持连接
-}
+// // 用户API接口,供前端用户调用
+// type userAPI interface {
+// 	apiLogin(c *gin.Context)         // 登录接口，返回用户上下文
+// 	apiGetAllBookIds(c *gin.Context) // 获取所有书籍 ID
+// 	apiGetBookInfo(c *gin.Context)   // 获取书籍信息
+// 	apiGetBookFile(c *gin.Context)   // 获取书籍文件
+// 	apiAlive(c *gin.Context)         // 保持连接
+// }
 
-// 管理员API接口,供前端管理员调用
-type adminAPI interface {
-	apiAuthenticate(c *gin.Context)   // 鉴权接口，返回用户上下文
-	apiAddBook(c *gin.Context)        // 添加书籍
-	apiUpdateBookInfo(c *gin.Context) // 更新书籍信息
-	apiDeleteBook(c *gin.Context)     // 删除书籍
-}
+// // 管理员API接口,供前端管理员调用
+// type adminAPI interface {
+// 	apiAuthenticate(c *gin.Context)   // 鉴权接口，返回用户上下文
+// 	apiAddBook(c *gin.Context)        // 添加书籍
+// 	apiUpdateBookInfo(c *gin.Context) // 更新书籍信息
+// 	apiDeleteBook(c *gin.Context)     // 删除书籍
+// }
 
 const ( // 用户/游客API地址
-	API_USER_LOGIN           string = "/api/booklibrary/login"
-	API_USER_GET_USER_INFO   string = "/api/booklibrary/getuserinfo"
-	API_USER_GET_BOOK_INFO   string = "/api/booklibrary/getbookinfo"
-	API_USE_GET_ALL_BOOK_IDS string = "/api/booklibrary/getallbookids"
-	API_ALIVE                string = "/api/booklibrary/"
+	API_USER_LOGIN               string = "/api/booklibrary/login"
+	API_USER_GET_USER_INFO       string = "/api/booklibrary/getuserinfo"
+	API_USER_GET_BOOK_INFO_BY_ID string = "/api/booklibrary/getbookinfobyid"
+	API_USE_GET_ALL_BOOK_IDS     string = "/api/booklibrary/getallbookids"
+	API_ALIVE                    string = "/api/booklibrary/"
 )
 
 const ( // 管理员/开发者API地址
@@ -50,7 +50,8 @@ func RegisterAPIs() {
 	color.Green("booklibrary::apis::RegisterAPIs()::开始注册了API...")
 
 	ginserver.Listen(API_ALIVE, apiAlive, "图书馆系统Alive可用性测试API")
-	ginserver.Listen(API_USE_GET_ALL_BOOK_IDS, apiGetAllBookIds, "获取书籍信息")
+	ginserver.Listen(API_USE_GET_ALL_BOOK_IDS, apiGetAllBookIds, "获取所有保存的书籍信息")
+	ginserver.Listen(API_USER_GET_BOOK_INFO_BY_ID, apiGetBookInfoById, "根据ID信息，精准获取书籍信息. 接受参数：book_id")
 
 	// 仅供开发者调试的API
 	ginserver.Listen(APIDEV_GET_ALL_BOOKS, apiDevGetAllBooks, "获取所有书籍信息")
@@ -78,6 +79,23 @@ func apiGetAllBookIds(c *gin.Context) {
 
 	// 返回书籍 ID
 	c.JSON(http.StatusOK, gin.H{"book_ids": strArr})
+}
+
+// 接受包内书籍ID参数，返回书籍信息 或者 错误信息
+func apiGetBookInfoById(c *gin.Context) {
+	// 获取书籍信息
+	bookID := c.Query("book_id")         // 获取书籍ID
+	book, err := GetBookInfoById(bookID) // Book, Error
+	fmt.Println("  读取书籍信息:", book, err)
+
+	if err != nil {
+		// 如果出错，返回错误信息
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// 返回书籍信息
+	c.JSON(http.StatusOK, gin.H{"book": book})
 }
 
 // DEV ONLY
